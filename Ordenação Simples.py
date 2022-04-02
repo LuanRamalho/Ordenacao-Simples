37e6244a360e0e928072f7af09ea351bf8f92d57
Luan Ramalho <32373552+LuanRamalho@users.noreply.github.com>
2022-04-02 14:29:24 -0300

    Add files via upload

---
+++ Ordenação Simples.py
@@ -0,0 +1,19 @@
+a1 = [1, -16, -44, 3, 4, 8 ,18, 10, -33, 6, 34, 55, -35, 43, 25]
+a2 = ["Mamão", "Abacaxi", "Morango", "Abacate", "Uva", "Limão", "Banana", "Maçã", "Papaia", "Lichia",
+      "Laranja", "Melancia", "Jaca", "Tomate", "Cereja"]
+
+
+a1.sort(reverse=True)
+print("Números ordenados em ordem decrescente: ",a1)
+
+a1.sort()
+print("Números ordenados em ordem crescente: ",a1)
+
+print("")
+print("")
+
+a2.sort(reverse=True)
+print("Nomes ordenados em ordem decrescente: ",a2)
+
+a2.sort()
+print("Nomes ordenados em ordem crescente: ",a2)
